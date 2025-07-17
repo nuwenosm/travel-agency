988d6e32d8c4d9d14512058fdf6c025dc9d759be
nuwenosm <nuwenosm@gmail.com>
2025-07-17 15:52:28 -0700

    implement auth

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 export default [
   layout('routes/admin/admin-layout.tsx', [
     route('dashboard', 'routes/admin/dashboard.tsx'),
-    route('all-user', 'routes/admin/all-user.tsx')
+    route('all-users', 'routes/admin/all-users.tsx')
   ]),
 ] satisfies RouteConfig;
---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,16 @@
+import { Header } from "components"
+
+const AllUsers = () => {
+  return (
+    <main className="dashboard wrapper">
+      <Header 
+        title="Trip Page"
+        description="Check out current users"
+      />
+
+      All Users Page Contents
+    </main>
+  )
+}
+
+export default AllUsers
